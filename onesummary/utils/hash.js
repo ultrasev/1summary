398c0362007df811edd021797dc8e435b318636c
ultrasev <i@ultrasev.com>
2024-10-28 10:39:34 +0800

    feat: add download button and functionality

---
+++ onesummary/utils/hash.js
@@ -0,0 +1,7 @@
+export async function generateHash(url) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(url);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('').slice(0, 8);
+}
